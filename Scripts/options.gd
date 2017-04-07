8de8701ab651b057d30149e0d4582c0eb4afdbce
William W Wold <wilcoe@rocketmail.com>
2017-04-06 21:11:57 -0700

    switched speed control to buttons

--- Scripts/options.gd
+++ Scripts/options.gd
@@ -15,6 +15,29 @@ func _on_combine_streaks_btn_toggled( pressed ):
 func _on_combine_loops_btn_toggled( pressed ):
 	controller.combineLoop = pressed
 
+func speed_btn_pressed(btn):
+	get_node("PanelContainer/VBoxContainer/HBoxContainer/speed_btn_1").set_pressed(btn == 1)
+	get_node("PanelContainer/VBoxContainer/HBoxContainer/speed_btn_2").set_pressed(btn == 2)
+	get_node("PanelContainer/VBoxContainer/HBoxContainer/speed_btn_3").set_pressed(btn == 3)
+	get_node("PanelContainer/VBoxContainer/HBoxContainer/speed_btn_4").set_pressed(btn == 4)
+	
+	if btn == 1:
+		controller.baseOpTime = controller.defaultBaseOpTime / 0.5
+	elif btn == 2:
+		controller.baseOpTime = controller.defaultBaseOpTime
+	elif btn == 3:
+		controller.baseOpTime = controller.defaultBaseOpTime / 4.0
+	else:
+		controller.baseOpTime = controller.defaultBaseOpTime / 20.0
+		
+func _on_speed_btn_1_toggled( pressed ):
+	speed_btn_pressed(1)
 
-func _on_HSlider_value_changed( value ):
-	controller.baseOpTime = (1-value) * 2 * controller.defaultBaseOpTime
+func _on_speed_btn_2_toggled( pressed ):
+	speed_btn_pressed(2)
+
+func _on_speed_btn_3_toggled( pressed ):
+	speed_btn_pressed(3)
+
+func _on_speed_btn_4_toggled( pressed ):
+	speed_btn_pressed(4)
